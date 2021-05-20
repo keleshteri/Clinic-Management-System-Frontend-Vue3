f0c0d9549cbf38e2808b4e5652e9c86a549ab892
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-20 18:40:49 +0800

    added table GoodTable

--- src/components/patients/PatientsList.vue
+++ src/components/patients/PatientsList.vue
@@ -1,12 +1,43 @@
 <template>
     <h1>list of Patients</h1>
-    <AutoTable></AutoTable>
+    <Table
+    :columnList="schema.columns"
+    :fields ="schema.fields"
+    :rows="rows"
+  max-height="300px"
+    ></Table>
+    {{ schema.fields }}
 </template>
 <script>
-import AutoTable from '../components/table-generator/AutoTable.vue'
+// import Table from '../../components/generator/table/Table.vue'
+import Table from '../../components/generator/good-table/Table.vue'
 export default {
     name:'PatientsList',
-    components:{AutoTable}
+    props:['schema'],
+    components:{Table},
+    data(){return{
+      // columns:{
+      //     name:{
+      //       label: 'Name',
+      //       field: 'name',
+      //     }
+      //   },
+       
+        rows:[
+               {
+                 id:1,
+                 name:"John",
+                //  age:20
+               },
+                {
+                 id:2,
+                 name:"ffff",
+                //  age:50
+               },
+ 
+            ],
+
+    }}
     
 }
 </script>
