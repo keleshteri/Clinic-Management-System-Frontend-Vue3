0f09439b10c63d630b095d3a0353c175cfabac45
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-05-20 18:41:10 +0800

    added  Table

---
+++ src/components/generator/form/Form.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>here form</h1>
+</template>
+<script>
+export default {
+    name:'Form'
+}
+</script>
--- src/components/generator/table/Table.vue
+++ src/components/generator/table/Table.vue
@@ -106,3 +106,8 @@
       </div>
     </div>
 </template>
+<script>
+export default {
+  name:'Table'
+}
+</script>
